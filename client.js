04bc27adef6e7c52303a28c846b7c6d912851b82
Nicholas Heilman <Nicholas.heilman@gmail.com>
2018-12-15 13:36:45 -0600

    input push to array

--- client.js
+++ client.js
@@ -2,6 +2,16 @@ console.log( 'JS' );
 
 $(readyNow); 
 
+function readyNow(){
+    console.log( 'jQuery' );
+    submitClick() ;
+};
+
+// function for button click 
+function submitClick(){
+    $('#submitButtonIn').on('click', click);
+};
+
 class Employee{
     constructor( firstName, lastName, idNumber, jobTitle, annualSalary){
     this.firstName = firstName;
@@ -12,19 +22,26 @@ class Employee{
     }
 }; // end Employee class 
 
-
-function readyNow(){
-    console.log( 'jQuery' );
-    submitClick() ;
-};
-
-// function for button click 
-function submitClick(){
-    $('#submitButton').on('click', click);
-};
-
+let employeeInfo = []; 
 // fuction for button click actions
 function click(){
     console.log('Submit click');
+    inputVals();
+    console.log(firstName, lastName,idNumber, jobTitle, annualSalary);
 };
+
+//get values from input 
+function inputVals(){
+firstName = $('#firstNameIn').val();
+lastName = $('#lastNameIn').val();
+idNumber = $('#idNumberIn').val();
+jobTitle = $('#jobTitleIn').val();
+annualSalary = $('#annualSalaryIn').val(); 
+};
+
+//function to add anualSalary to got monthly costs
+function monthltCost(){
+    
+}
+
 
