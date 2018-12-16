3337f78654d12115251bd54b1cba120667a868e5
Nicholas Heilman <Nicholas.heilman@gmail.com>
2018-12-16 13:55:47 -0600

    monthlyCost calc

--- client.js
+++ client.js
@@ -1,15 +1,9 @@
 console.log( 'JS' );
-
 $(readyNow); 
 
 function readyNow(){
     console.log( 'jQuery' );
     submitClick() ;
-};
-
-// function for button click 
-function submitClick(){
-    $('#submitButtonIn').on('click', click);
 };
 
 class Employee{
@@ -22,32 +16,58 @@ class Employee{
     }
 }; // end Employee class 
 
+// function for button click 
+function submitClick(){
+    $('#submitButtonIn').on('click', click);
+};
+
 let employeeInfo = []; 
+let annualCost = 0;
+
 // fuction for button click actions
 function click(){
     console.log('Submit click');
     inputVals();
-    console.log(firstName, lastName,idNumber, jobTitle, annualSalary);
+    emptyInputIn();
+    monthlyCost();
 }; // end button click
 
 //get values from input and push to array and class
 function inputVals(){
-firstName = $('#firstNameIn').val();
-lastName = $('#lastNameIn').val();
-idNumber = $('#idNumberIn').val();
-jobTitle = $('#jobTitleIn').val();
-annualSalary = $('#annualSalaryIn').val(); 
-let employ = new Employee( firstName, lastName, idNumber, jobTitle, annualSalary);
-employeeInfo.push( employ );
-return employ;
+    let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let idNumber = $('#idNumberIn').val();
+    let jobTitle = $('#jobTitleIn').val();
+    let annualSalary = parseFloat($('#annualSalaryIn').val()); 
+    let employ = new Employee( firstName, lastName, idNumber, jobTitle, annualSalary);
+    employeeInfo.push( employ );
+    annualCost += annualSalary;
+    return employ;
 }; // end inputVals 
 
+//function to add annual salaries
+function monthlyCost(){
+    let monthCost = annualCost / 12;
+    console.log( monthCost );
+}
 
-//function to add anualSalary to got monthly costs
-// function monthltCost(){
-//     employeeInfo.forEach(function(e) {
-//         if (userinputid == e.id) alert(e.price);
-//       });
+
+//function to add employeeInfo to Table on DOM
+// function newEEToTable() {
+//     $('.bodyEETable').empty();
+//     for (employee of staff){
+//         $('.bodyEETable').append('<tr class="emp"><td>' + employee.firstName + '</td><td>' + employee.lastName + '</td><td>' + employee.empID + '</td><td>' + employee.empTitle + '</td><td>$' + employee.empSalary + '</td></tr>')
+//     };
 // }
 
 
+
+
+  function emptyInputIn(){
+      // clear the inputs 
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idNumberIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
+  };
